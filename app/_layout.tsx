3a243d5f164d8d7c3e284ec5ace9bcbce6425873
Maaz-Ur-Rehman <maazurrehman42@gmail.com>
2025-04-15 12:37:46 +0500

    add code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,27 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Linking, StyleSheet } from 'react-native';
+import { Linking, Platform, StyleSheet } from 'react-native';
 import WebView from 'react-native-webview';
 
 export default function RootLayout() {
-  const handleShouldStartLoadWithRequest = (event: any) => {
-    if (event.url.includes("accounts.google.com")) {
-      Linking.openURL(event.url); // Open in Safari
-      return false; // Stop WebView from loading Google login
-    }
-    return true;
-  };
+  // const handleNavigationStateChange = (event: any) => {
+  //   // If URL is Google Sign-In, open it in external browser
+  //   if (event.url.includes("accounts.google.com")) {
+  //     Linking.openURL(event.url);
+  //   }
+  // };
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <WebView
-        source={{ uri: "https://infomary.com/login" }}
-        style={styles.container}
-        javaScriptEnabled
-        domStorageEnabled
-        useWebKit
-        originWhitelist={['*']}
-        onShouldStartLoadWithRequest={handleShouldStartLoadWithRequest}
-      />
+  source={{ uri: "https://infomary.com/login" }}
+  style={styles.container}
+  javaScriptEnabled
+  domStorageEnabled
+  scalesPageToFit
+  useWebKit
+  originWhitelist={['*']}
+  userAgent={Platform.OS === 'android' ? 'Chrome/18.0.1025.133 Mobile Safari/535.19' : 'AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75'}/>
+
     </SafeAreaView>
   );
 }
